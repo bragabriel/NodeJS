4c95d46e748bfb4d4b3d18c6cdcaf863fab83e30
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-25 13:45:53 -0300

    Add: introdução ao Express

---
+++ Curso-NodeJS/Aula06_IntroducaoExpress/app.js
@@ -0,0 +1,10 @@
+// NPM  ->  Maior repositório de pacotes que existe
+// Express  ->  Framework utilizado para criar aplicações back-end usando Node.JS
+
+/*
+No terminal:
+
+> npm -v (verificar a versão caso esteja instalado)
+> npm install express --save (--save: salvar os arquivos do módulo dentro do proprio projeto) 
+
+*/
