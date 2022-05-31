8ea35500234b7455de81284c8f47ac92126b576e
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-31 14:14:35 -0300

    add: definindo a configuração base

--- Curso-NodeJS/Modulo03/index.js
+++ Curso-NodeJS/Modulo03/index.js
@@ -5,12 +5,34 @@ const app = express();
 //variável app está recebendo a função express() que vem do módulo express
 //Ou seja, qualquer coisa que vamos usar do express, vamos usar a partir da variável app
 
+const handlebars = require('express-handlebars');
+//carregando o módulo handlebars do express para a variável handlebars
+
+const Sequelize = require('sequelize');
 
 
-// Abrindo um servidor usando express:
-// - Primeiro executa o app.listen 8081
-// - Depois executa a função (que no exemplo, é para exibir uma msg de servidor rodando)
-app.listen(8081, function(){
-    console.log("Servidor Rodando na URL http://localhost:8081");
-})
+
+/* Config */
+    
+    /* Template Engine */
+
+    //usando o handlebars como a nossa template-engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'})); //main: template padrão dessa aplicação
+    app.set('view engine', 'handlebars');
+
+
+    /* Conexão com o DB */
+    const sequelize = new Sequelize('dbteste', 'root', '1234', {
+        //                           db,      user,   senha,  {host -> Onde está rodando}
+        host: "localhost",
+        dialect: 'mysql'  //tipo de db que queremos nos conectar
+    });
+
+
+/* Server */
+    // - Primeiro executa o app.listen 8081
+    // - Depois executa a função (que no exemplo, é para exibir uma msg de servidor rodando)
+    app.listen(8081, function(){
+        console.log("Servidor Rodando na URL http://localhost:8081");
+    })
 
