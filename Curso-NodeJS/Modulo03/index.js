ba723e431beb0a3e9846e5fba728cb88dedc45cf
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-31 14:06:00 -0300

    add: express-handlebars

--- Curso-NodeJS/Modulo03/index.js
+++ Curso-NodeJS/Modulo03/index.js
@@ -6,26 +6,6 @@ const app = express();
 //Ou seja, qualquer coisa que vamos usar do express, vamos usar a partir da variável app
 
 
-app.get("/", function(req, res){
-    res.send("Seja bem-vindo ao meu app!");
-})
-
-app.get("/sobre", function(req, res){
-    res.send("Minha pagina Sobre!");
-})
-
-app.get("/blog", function(req, res){
-    res.send("Meu Blog!");
-})
-
-app.get("/liveTeste", function(req, res){
-    res.send("BIRL!");
-})
-
-app.get("/rodaTeste", function(req, res){
-    res.send("asdasdasdasdasd");
-})
-
 
 // Abrindo um servidor usando express:
 // - Primeiro executa o app.listen 8081
