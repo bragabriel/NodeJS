88c03149b2018142afcf8773e27a316c6274bacc
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-13 16:12:18 -0300

    style: update

--- Curso-NodeJS/Modulo05/routes/admin.js
+++ Curso-NodeJS/Modulo05/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
-    res.render("admin/index")
+    res.render('admin/index')
 })
 
 router.get('/posts', (req, res)=>{
@@ -10,7 +10,11 @@ router.get('/posts', (req, res)=>{
 })
 
 router.get('/categorias', (req, res)=>{
-    res.send("Página de categorias")
+    res.render('admin/categorias')
+})
+
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/addcategoria')
 })
 
 //exportando nossas rotas
