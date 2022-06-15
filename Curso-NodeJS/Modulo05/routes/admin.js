710b62a363ff0d867a9fffeed57de0a3a6049817
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-15 15:00:36 -0300

    feat: category edit

--- Curso-NodeJS/Modulo05/routes/admin.js
+++ Curso-NodeJS/Modulo05/routes/admin.js
@@ -13,7 +13,13 @@ router.get('/posts', (req, res)=>{
 })
 
 router.get('/categorias', (req, res)=>{
-    res.render('admin/categorias')
+    Categorias.find().lean().sort({date: 'desc'}).then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias")
+        res.redirect('/admin')
+    })
+   
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -63,6 +69,60 @@ router.post('/categorias/nova', (req, res) => {
         })
     }    
 })
+
+router.get("/categorias/edit/:id", (req, res) => {
+    Categorias.findOne({_id:req.params.id}).lean().then((categoria) => {
+        res.render("admin/editcategorias", {categoria: categoria})
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe.")
+        res.redirect("/admin/categorias")
+    })
+   
+})
+
+
+router.post("/categorias/edit", (req, res) => {
+    Categorias.findOne({ _id: req.body.id }).then((categoria) => {
+        let erros = []
+
+        if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+            erros.push({ texto: "Nome invalido" })
+        }
+        if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+            erros.push({ texto: "Slug invalido" })
+        }
+        if (req.body.nome.length < 2) {
+            erros.push({ texto: "Nome da categoria muito pequeno" })
+        }
+        if (erros.length > 0) {
+            Categoria.findOne({ _id: req.body.id }).lean().then((categoria) => {
+                res.render("admin/editcategorias", { categoria: categoria})
+            }).catch((err) => {
+                req.flash("error_msg", "Erro ao pegar os dados")
+                res.redirect("admin/categorias")
+            })
+            
+        } else {
+
+
+            categoria.nome = req.body.nome
+            categoria.slug = req.body.slug
+
+            categoria.save().then(() => {
+                req.flash("success_msg", "Categoria editada com sucesso!")
+                res.redirect("/admin/categorias")
+            }).catch((err) => {
+                req.flash("error_msg", "Erro ao salvar a edição da categoria")
+                res.redirect("admin/categorias")
+            })
+
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Erro ao editar a categoria")
+        res.redirect("/admin/categorias")
+    })
+})
+
 
 //exportando nossas rotas
 module.exports = router
