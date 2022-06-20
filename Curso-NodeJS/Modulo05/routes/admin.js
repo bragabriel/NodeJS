af90ea21944228cb6f69a57735f170f53b820454
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-20 11:19:38 -0300

    feat: post edit

--- Curso-NodeJS/Modulo05/routes/admin.js
+++ Curso-NodeJS/Modulo05/routes/admin.js
@@ -181,8 +181,51 @@ router.post("/postagens/nova", (req, res) => {
         }).catch((err) => {
             req.flash("error_msg", "Houve um erro durante o salvamento da postagem")
             res.redirect("/admin/postagens")
+            console.log(err)
         })
     }
+})
+
+router.get("/postagens/edit/:id", (req, res) => {
+
+    Postagem.findOne({_id: req.params.id}).lean().then((postagem) => {
+
+        Categorias.find().lean().then((categorias) => {
+            res.render("admin/editpostagens", {categorias: categorias, postagem: postagem})
+
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.redirect("/admin/postagens")
+            console.log(err)
+        })
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário de edição")
+        res.redirect("/admin/postagens")
+        console.log(err)
+    })
+})
+
+router.post("/postagem/edit", (req, res) => {
+    Postagem.findOne({_id: req.body.id}).then((postagem) => {
+
+        postagem.titulo = req.body.titulo
+        postagem.slug = req.body.slug
+        postagem.descricao = req.body.descricao
+        postagem.conteudo = req.body.conteudo
+        postagem.categoria = req.body.categoria
+
+        postagem.save().then(() => {
+            req.flash("success_msg", "Postagem editada com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao editar a postagem")
+            res.redirect("/admin/postagens")
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao salvar a edição")
+        res.redirect("/admin/postagens")
+    })
 })
 
 //exportando nossas rotas
